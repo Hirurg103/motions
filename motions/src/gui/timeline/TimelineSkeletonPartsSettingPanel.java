0bd956693b1fd1f09533c8b3e7d65028c9035692
Dmitry Kovalenok <dzm.kov@gmail.com>
2013-05-11 00:54:25 +0300

    Create timeline skeleton parts setting panel

---
+++ motions/src/gui/timeline/TimelineSkeletonPartsSettingPanel.java
@@ -0,0 +1,17 @@
+package gui.timeline;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+public class TimelineSkeletonPartsSettingPanel extends JPanel {
+
+	/**
+	 * 	Holds time line settings for all skeleton parts.
+	 */
+	private static final long serialVersionUID = -4311210569005609359L;
+	
+	public TimelineSkeletonPartsSettingPanel() {
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	}
+}
