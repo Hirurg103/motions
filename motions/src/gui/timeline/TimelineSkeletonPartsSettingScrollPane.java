a48ab0852d153a4db8cffba6db9b8f5b41d6c176
Dmitry Kovalenok <dzm.kov@gmail.com>
2013-05-13 00:17:14 +0300

    Add timeline skeleton parts setting scroll pane

---
+++ motions/src/gui/timeline/TimelineSkeletonPartsSettingScrollPane.java
@@ -0,0 +1,31 @@
+package gui.timeline;
+
+import java.awt.Rectangle;
+
+import javax.swing.JScrollPane;
+
+public class TimelineSkeletonPartsSettingScrollPane extends JScrollPane {
+
+	/**
+	 * 	Scroll pane to display skeleton parts setting panel
+	 */
+	private static final long serialVersionUID = -4133745910505408563L;
+
+	public TimelineSkeletonPartsSettingScrollPane() { 
+		super();
+	}
+
+	public void resetHorisontalScrollBarPosition(int mousePosition) {
+		Rectangle visibleArea = TimelinePanel.timelineSkeletonPartsSettingPanel.getVisibleRect();
+		if(mousePosition < visibleArea.x) {
+			getHorizontalScrollBar().setValue(Math.max(0, mousePosition));
+		} else {
+			int newSliderPosition = mousePosition - visibleArea.width + TimelineMotionDimension.horizontalSliderOffset(); 
+			if(visibleArea.x < newSliderPosition) {
+				getHorizontalScrollBar().setValue(Math.min(TimelineDimensionSettingPanel.timelineDimensionSettingPanelWidth, newSliderPosition));
+				TimelineDimensionSettingPanel.timelineDimensionSettingPanelWidth = mousePosition + TimelineMotionDimension.horizontalSliderOffset();
+				TimelinePanel.timelineSkeletonPartsSettingPanel.revalidate();
+			}
+		}
+	}
+}
